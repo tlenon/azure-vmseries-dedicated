99cb551af85c18d0bc67aad2248dab5f49391acc
Tony Lenon <46562678+tlenon@users.noreply.github.com>
2023-11-07 10:17:21 +0000

    Update main.tf
    Add route propagation support

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -90,10 +90,11 @@ resource "azurerm_network_security_rule" "this" {
 resource "azurerm_route_table" "this" {
   for_each = var.route_tables
 
-  name                = "${var.name_prefix}${each.value.name}"
-  location            = try(each.value.location, var.location)
-  resource_group_name = var.resource_group_name
-  tags                = var.tags
+  name                          = "${var.name_prefix}${each.value.name}"
+  disable_bgp_route_propagation = try(each.value.disable_bgp_route_propagation, each.value == 0 ? false : true)
+  location                      = try(each.value.location, var.location)
+  resource_group_name           = var.resource_group_name
+  tags                          = var.tags
 }
 
 locals {
